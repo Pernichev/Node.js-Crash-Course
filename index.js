cf56b2f7959acef4ef1ce4aebe70cfeb40135eb1
Pernichev <pernichevm@gmail.com>
2019-07-14 15:36:39 +0300

    added events and logger

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const Person = require('./person.js');
 
 const person = new Person('John Doe', 42);
 
+const Logger = require('./logger');
+
 person.greeting();
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener: ', data));
+
+logger.log('Hello world');
